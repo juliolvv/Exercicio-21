d5627a675ef5be383d687c28d95b380f5e7e90f4
Júlio César <juliocesarolv13@gmail.com>
2025-05-14 18:12:40 -0300

    First Commit

---
+++ src/main/java/com/mycompany/exercicio21/Exercicio21.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio21;
+import javax.swing.JOptionPane;
+
+public class Exercicio21 {
+    public static void main(String[] args) {
+        
+        for (int i = 34; i <57;i=i+2) {
+            
+                JOptionPane.showMessageDialog(null,"valor: " + i);
+            }
+        }
+    }
